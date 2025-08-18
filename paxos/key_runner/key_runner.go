7db59cebdc305799ce805788a0b940f32fecc96c
QuangTung97 <quangtung29121997@gmail.com>
2025-08-18 10:07:19 +0700

    Add Generic Key Runner

---
+++ paxos/key_runner/key_runner.go
@@ -0,0 +1,172 @@
+package key_runner
+
+import (
+	"context"
+	"sync"
+)
+
+// New creates an object that manages a set of runners based on keys and values.
+// When a new key is added / removed => a runner will be started / cancelled accordingly.
+// When the value of a key is changed => the runner that is associated with that key will also be restarted.
+func New[K comparable, V comparable](
+	getKey func(V) K,
+	handler func(ctx context.Context, val V),
+) *KeyRunner[K, V] {
+	return &KeyRunner[K, V]{
+		getKey:  getKey,
+		handler: handler,
+
+		activeKeys: map[K]struct{}{},
+		running:    map[K]*runThread[V]{},
+	}
+}
+
+type KeyRunner[K comparable, V comparable] struct {
+	getKey  func(V) K
+	handler func(ctx context.Context, val V)
+
+	mut        sync.Mutex
+	activeKeys map[K]struct{}
+	running    map[K]*runThread[V]
+
+	wg sync.WaitGroup
+}
+
+// ==================================
+// Public Methods
+// ==================================
+
+func (r *KeyRunner[K, V]) Upsert(values []V) {
+	startList := r.upsertInternal(values)
+
+	for _, tmp := range startList {
+		entry := tmp
+		r.wg.Add(1)
+
+		go func() {
+			defer r.wg.Done()
+			r.doRunHandler(entry)
+		}()
+	}
+}
+
+func (r *KeyRunner[K, V]) Shutdown() {
+	r.upsertInternal(nil)
+	r.wg.Wait()
+}
+
+// ==================================
+// Private Methods
+// ==================================
+
+func (r *KeyRunner[K, V]) doRunHandler(entry startEntry[V]) {
+	for {
+		r.handler(entry.ctx, entry.val)
+
+		key := r.getKey(entry.val)
+
+		var continued bool
+		entry, continued = r.finishInternal(key)
+		if !continued {
+			return
+		}
+	}
+}
+
+type runThread[V comparable] struct {
+	val    V
+	cancel func()
+}
+
+type startEntry[V comparable] struct {
+	val V
+	ctx context.Context
+}
+
+func (r *KeyRunner[K, V]) upsertInternal(values []V) []startEntry[V] {
+	r.mut.Lock()
+	defer r.mut.Unlock()
+
+	var startList []startEntry[V]
+
+	newSet := map[K]struct{}{}
+
+	for _, val := range values {
+		key := r.getKey(val)
+		newSet[key] = struct{}{}
+	}
+
+	var deleteKeys []K
+	for key := range r.activeKeys {
+		_, ok := newSet[key]
+		if ok {
+			continue
+		}
+
+		deleteKeys = append(deleteKeys, key)
+		thread := r.running[key]
+		thread.cancel()
+	}
+
+	for _, key := range deleteKeys {
+		delete(r.activeKeys, key)
+	}
+
+	for _, val := range values {
+		key := r.getKey(val)
+
+		_, existed := r.activeKeys[key]
+		if existed {
+			// update on changed
+			thread := r.running[key]
+			if thread.val != val {
+				thread.val = val
+				thread.cancel()
+			}
+			continue
+		}
+		r.activeKeys[key] = struct{}{}
+
+		thread, ok := r.running[key]
+		if ok {
+			thread.val = val
+			continue
+		}
+
+		// do insert new
+		ctx, cancel := context.WithCancel(context.Background())
+
+		r.running[key] = &runThread[V]{
+			val:    val,
+			cancel: cancel,
+		}
+
+		startList = append(startList, startEntry[V]{
+			val: val,
+			ctx: ctx,
+		})
+	}
+
+	return startList
+}
+
+func (r *KeyRunner[K, V]) finishInternal(key K) (startEntry[V], bool) {
+	r.mut.Lock()
+	defer r.mut.Unlock()
+
+	_, ok := r.activeKeys[key]
+	if !ok {
+		delete(r.running, key)
+		return startEntry[V]{}, false
+	}
+
+	thread := r.running[key]
+
+	ctx, cancel := context.WithCancel(context.Background())
+	thread.cancel = cancel
+
+	return startEntry[V]{
+		val: thread.val,
+		ctx: ctx,
+	}, true
+}
---
+++ paxos/key_runner/key_runner_test.go
@@ -0,0 +1,289 @@
+package key_runner
+
+import (
+	"cmp"
+	"context"
+	"slices"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type objectValue struct {
+	key string
+	val int
+}
+
+func (o objectValue) getKey() string {
+	return o.key
+}
+
+func clearContexts(list []startEntry[objectValue]) []startEntry[objectValue] {
+	list = slices.Clone(list)
+	for i := range list {
+		list[i].ctx = nil
+	}
+	return list
+}
+
+type orderedAndCmp interface {
+	comparable
+	cmp.Ordered
+}
+
+func getKeys[K orderedAndCmp, V any](m map[K]V) []K {
+	result := make([]K, 0, len(m))
+	for k := range m {
+		result = append(result, k)
+	}
+	slices.Sort(result)
+	return result
+}
+
+func TestKeyRunner__Upsert_With_Remove(t *testing.T) {
+	r := New(objectValue.getKey, nil)
+
+	startList := r.upsertInternal([]objectValue{
+		{key: "key01", val: 11},
+		{key: "key02", val: 12},
+		{key: "key03", val: 13},
+	})
+
+	// check start list
+	assert.Equal(t, []startEntry[objectValue]{
+		{val: objectValue{key: "key01", val: 11}},
+		{val: objectValue{key: "key02", val: 12}},
+		{val: objectValue{key: "key03", val: 13}},
+	}, clearContexts(startList))
+
+	// no context is canceled
+	assert.Equal(t, nil, startList[0].ctx.Err())
+	assert.Equal(t, nil, startList[2].ctx.Err())
+
+	// remove key 01, key 02
+	startList2 := r.upsertInternal([]objectValue{
+		{key: "key03", val: 13},
+	})
+	assert.Equal(t, 0, len(startList2))
+
+	// check context canceled
+	assert.Equal(t, context.Canceled, startList[0].ctx.Err())
+	assert.Equal(t, context.Canceled, startList[1].ctx.Err())
+	assert.Equal(t, nil, startList[2].ctx.Err())
+
+	// check active key
+	assert.Equal(t, map[string]struct{}{
+		"key03": {},
+	}, r.activeKeys)
+
+	// check running thread
+	assert.Equal(t, []string{"key01", "key02", "key03"}, getKeys(r.running))
+}
+
+func TestKeyRunner__Upsert__Then_Remove__Then_Finish(t *testing.T) {
+	r := New(objectValue.getKey, nil)
+
+	startList := r.upsertInternal([]objectValue{
+		{key: "key01", val: 11},
+		{key: "key02", val: 12},
+	})
+
+	// check start list
+	assert.Equal(t, []startEntry[objectValue]{
+		{val: objectValue{key: "key01", val: 11}},
+		{val: objectValue{key: "key02", val: 12}},
+	}, clearContexts(startList))
+
+	// no context is canceled
+	assert.Equal(t, nil, startList[0].ctx.Err())
+	assert.Equal(t, nil, startList[1].ctx.Err())
+
+	// remove key01
+	startList2 := r.upsertInternal([]objectValue{
+		{key: "key02", val: 12},
+	})
+	assert.Equal(t, 0, len(startList2))
+
+	// check context
+	assert.Equal(t, context.Canceled, startList[0].ctx.Err())
+	assert.Equal(t, nil, startList[1].ctx.Err())
+
+	// finish key01
+	startEntry1, ok := r.finishInternal("key01")
+	assert.Equal(t, false, ok)
+	assert.Equal(t, startEntry[objectValue]{}, startEntry1)
+
+	// check active key
+	assert.Equal(t, map[string]struct{}{
+		"key02": {},
+	}, r.activeKeys)
+
+	// check running thread
+	assert.Equal(t, []string{"key02"}, getKeys(r.running))
+}
+
+func TestKeyRunner__Update_Value__Then_Finish(t *testing.T) {
+	r := New(objectValue.getKey, nil)
+
+	startList := r.upsertInternal([]objectValue{
+		{key: "key01", val: 11},
+		{key: "key02", val: 12},
+	})
+
+	// check start list
+	assert.Equal(t, []startEntry[objectValue]{
+		{val: objectValue{key: "key01", val: 11}},
+		{val: objectValue{key: "key02", val: 12}},
+	}, clearContexts(startList))
+
+	// do update
+	startList2 := r.upsertInternal([]objectValue{
+		{key: "key01", val: 21},
+		{key: "key02", val: 22},
+	})
+	assert.Equal(t, 0, len(startList2))
+
+	// check context canceled
+	assert.Equal(t, context.Canceled, startList[0].ctx.Err())
+	assert.Equal(t, context.Canceled, startList[1].ctx.Err())
+
+	// finish key01
+	startEntry3, ok := r.finishInternal("key01")
+	assert.Equal(t, true, ok)
+
+	// check start entry
+	assert.Equal(t, nil, startEntry3.ctx.Err())
+	startEntry3.ctx = nil
+	assert.Equal(t, startEntry[objectValue]{
+		val: objectValue{key: "key01", val: 21},
+	}, startEntry3)
+
+	// check active key
+	assert.Equal(t, map[string]struct{}{
+		"key01": {},
+		"key02": {},
+	}, r.activeKeys)
+
+	// check running thread
+	assert.Equal(t, []string{"key01", "key02"}, getKeys(r.running))
+}
+
+func TestKeyRunner__Remove_Then_Add_Again__Before_Finish(t *testing.T) {
+	r := New(objectValue.getKey, nil)
+
+	startList := r.upsertInternal([]objectValue{
+		{key: "key01", val: 11},
+		{key: "key02", val: 12},
+	})
+
+	// check start list
+	assert.Equal(t, []startEntry[objectValue]{
+		{val: objectValue{key: "key01", val: 11}},
+		{val: objectValue{key: "key02", val: 12}},
+	}, clearContexts(startList))
+
+	// remove key01
+	startList2 := r.upsertInternal([]objectValue{
+		{key: "key02", val: 12},
+	})
+	assert.Equal(t, 0, len(startList2))
+
+	// add again & updated
+	startList3 := r.upsertInternal([]objectValue{
+		{key: "key01", val: 21},
+		{key: "key02", val: 12},
+	})
+	assert.Equal(t, 0, len(startList3))
+
+	// check context canceled
+	assert.Equal(t, context.Canceled, startList[0].ctx.Err())
+	assert.Equal(t, nil, startList[1].ctx.Err())
+
+	// check active key
+	assert.Equal(t, map[string]struct{}{
+		"key01": {},
+		"key02": {},
+	}, r.activeKeys)
+
+	// check running thread
+	assert.Equal(t, []string{"key01", "key02"}, getKeys(r.running))
+
+	// finish key01
+	entry1, ok := r.finishInternal("key01")
+	assert.Equal(t, true, ok)
+	assert.Equal(t, nil, entry1.ctx.Err())
+	assert.Equal(t, objectValue{key: "key01", val: 21}, entry1.val)
+}
+
+func TestKeyRunner_Public(t *testing.T) {
+	var runningMut sync.Mutex
+	runningSet := map[string]objectValue{}
+
+	r := New(
+		objectValue.getKey,
+		func(ctx context.Context, val objectValue) {
+			runningMut.Lock()
+			runningSet[val.key] = val
+			runningMut.Unlock()
+
+			<-ctx.Done()
+
+			runningMut.Lock()
+			delete(runningSet, val.key)
+			runningMut.Unlock()
+		},
+	)
+
+	// insert
+	r.Upsert([]objectValue{
+		{key: "key01", val: 11},
+		{key: "key02", val: 12},
+	})
+
+	// check running set
+	time.Sleep(50 * time.Millisecond)
+	runningMut.Lock()
+	assert.Equal(t, map[string]objectValue{
+		"key01": {key: "key01", val: 11},
+		"key02": {key: "key02", val: 12},
+	}, runningSet)
+	runningMut.Unlock()
+
+	// delete key01
+	r.Upsert([]objectValue{
+		{key: "key02", val: 12},
+	})
+
+	// check running set
+	time.Sleep(50 * time.Millisecond)
+	runningMut.Lock()
+	assert.Equal(t, map[string]objectValue{
+		"key02": {key: "key02", val: 12},
+	}, runningSet)
+	runningMut.Unlock()
+
+	// update key 02 & insert key 03
+	r.Upsert([]objectValue{
+		{key: "key02", val: 22},
+		{key: "key03", val: 13},
+	})
+
+	// check running set
+	time.Sleep(50 * time.Millisecond)
+	runningMut.Lock()
+	assert.Equal(t, map[string]objectValue{
+		"key02": {key: "key02", val: 22},
+		"key03": {key: "key03", val: 13},
+	}, runningSet)
+	runningMut.Unlock()
+
+	r.Shutdown()
+
+	// check running set after shutdown
+	time.Sleep(50 * time.Millisecond)
+	runningMut.Lock()
+	assert.Equal(t, map[string]objectValue{}, runningSet)
+	runningMut.Unlock()
+}
