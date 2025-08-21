bf42617bf71b46669c6e9450107c6efaa6479461
quangtung97 <quangtung29121997@gmail.com>
2025-08-21 10:20:40 +0700

    Basic Handle Request Vote

--- paxos/acceptor.go
+++ paxos/acceptor.go
@@ -2,40 +2,84 @@ package paxos
 
 import (
 	"iter"
+	"sync"
 )
 
 type AcceptorLogic interface {
-	HandleRequestVote(input RequestVoteInput) iter.Seq[RequestVoteOutput]
-	AcceptEntries(input AcceptEntriesInput) AcceptEntriesOutput
+	HandleRequestVote(input RequestVoteInput) (iter.Seq[RequestVoteOutput], error)
+	AcceptEntries(input AcceptEntriesInput) (AcceptEntriesOutput, error)
 }
 
 type acceptorLogicImpl struct {
+	mut sync.Mutex
 	log LogStorage
+
+	limit int
 }
 
 func NewAcceptorLogic(
 	log LogStorage,
+	limit int,
 ) AcceptorLogic {
 	return &acceptorLogicImpl{
-		log: log,
+		log:   log,
+		limit: limit,
 	}
 }
 
-func (s *acceptorLogicImpl) HandleRequestVote(input RequestVoteInput) iter.Seq[RequestVoteOutput] {
+func (s *acceptorLogicImpl) HandleRequestVote(
+	input RequestVoteInput,
+) (iter.Seq[RequestVoteOutput], error) {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	// TODO check input node id
+
 	return func(yield func(RequestVoteOutput) bool) {
-		yield(RequestVoteOutput{
-			Success: true,
-			Term:    input.Term,
-			Entries: []VoteLogEntry{
-				{
-					Pos:     input.FromPos,
+		fromPos := input.FromPos
+
+		for {
+			entries := s.log.GetEntries(fromPos, s.limit)
+
+			voteEntries := make([]VoteLogEntry, 0, len(entries))
+			for _, e := range entries {
+				voteEntries = append(voteEntries, VoteLogEntry{
+					Pos:     e.Pos,
+					IsFinal: false,
+					Entry:   e.Entry,
+				})
+			}
+
+			isFinal := len(entries) < s.limit
+			newFromPos := fromPos + LogPos(len(entries))
+
+			if isFinal {
+				voteEntries = append(voteEntries, VoteLogEntry{
+					Pos:     newFromPos,
 					IsFinal: true,
-				},
-			},
-		})
-	}
+				})
+			}
+
+			ok := yield(RequestVoteOutput{
+				Success: true,
+				Term:    input.Term,
+				Entries: voteEntries,
+			})
+			if !ok {
+				return
+			}
+
+			if isFinal {
+				return
+			}
+
+			fromPos = newFromPos
+		}
+	}, nil
 }
 
-func (s *acceptorLogicImpl) AcceptEntries(input AcceptEntriesInput) AcceptEntriesOutput {
-	return AcceptEntriesOutput{}
+func (s *acceptorLogicImpl) AcceptEntries(
+	input AcceptEntriesInput,
+) (AcceptEntriesOutput, error) {
+	return AcceptEntriesOutput{}, nil
 }
--- paxos/acceptor_test.go
+++ paxos/acceptor_test.go
@@ -24,31 +24,37 @@ func newAcceptorLogicTest() *acceptorLogicTest {
 		NodeID: nodeID1,
 	}
 
+	s.putMembers()
+	s.initLogic(100)
+
+	return s
+}
+
+func (s *acceptorLogicTest) putMembers() {
 	memberEntry := NewMembershipLogEntry(
 		InfiniteTerm{},
 		[]MemberInfo{
 			{Nodes: []NodeID{nodeID1, nodeID2, nodeID3}, CreatedAt: 1},
 		},
 	)
-
 	s.log.UpsertEntries([]PosLogEntry{
 		{Pos: 1, Entry: memberEntry},
 	})
-	s.initLogic()
-
-	return s
 }
 
-func (s *acceptorLogicTest) initLogic() {
-	s.logic = NewAcceptorLogic(s.log)
+func (s *acceptorLogicTest) initLogic(limit int) {
+	s.logic = NewAcceptorLogic(s.log, limit)
 }
 
 func (s *acceptorLogicTest) doHandleVote(term TermNum, fromPos LogPos) []RequestVoteOutput {
-	it := s.logic.HandleRequestVote(RequestVoteInput{
+	it, err := s.logic.HandleRequestVote(RequestVoteInput{
 		ToNode:  nodeID2,
 		Term:    term,
 		FromPos: fromPos,
 	})
+	if err != nil {
+		panic("Should handle req vote ok, but got: " + err.Error())
+	}
 
 	var outputs []RequestVoteOutput
 	for x := range it {
@@ -57,7 +63,22 @@ func (s *acceptorLogicTest) doHandleVote(term TermNum, fromPos LogPos) []Request
 	return outputs
 }
 
-func TestAcceptorLogic_HandleRequestVote(t *testing.T) {
+func newPosLogEntries(from LogPos, entries ...LogEntry) []PosLogEntry {
+	result := make([]PosLogEntry, 0, len(entries))
+	for index, e := range entries {
+		result = append(result, PosLogEntry{
+			Pos:   from + LogPos(index),
+			Entry: e,
+		})
+	}
+	return result
+}
+
+func (s *acceptorLogicTest) newCmd(cmd string) LogEntry {
+	return NewCmdLogEntry(s.currentTerm.ToInf(), []byte(cmd))
+}
+
+func TestAcceptorLogic_HandleRequestVote__No_Log_Entries(t *testing.T) {
 	s := newAcceptorLogicTest()
 
 	outputs := s.doHandleVote(s.currentTerm, 2)
@@ -70,6 +91,72 @@ func TestAcceptorLogic_HandleRequestVote(t *testing.T) {
 					Pos:     2,
 					IsFinal: true,
 				},
+			},
+		},
+	}, outputs)
+}
+
+func TestAcceptorLogic_HandleRequestVote__With_Log_Entries(t *testing.T) {
+	s := newAcceptorLogicTest()
+
+	s.putMembers()
+
+	s.log.UpsertEntries(
+		newPosLogEntries(2,
+			s.newCmd("cmd test 01"),
+			s.newCmd("cmd test 02"),
+			s.newCmd("cmd test 03"),
+		),
+	)
+
+	s.initLogic(100)
+
+	outputs := s.doHandleVote(s.currentTerm, 2)
+	assert.Equal(t, []RequestVoteOutput{
+		{
+			Success: true,
+			Term:    s.currentTerm,
+			Entries: []VoteLogEntry{
+				{Pos: 2, Entry: s.newCmd("cmd test 01")},
+				{Pos: 3, Entry: s.newCmd("cmd test 02")},
+				{Pos: 4, Entry: s.newCmd("cmd test 03")},
+				{Pos: 5, IsFinal: true},
+			},
+		},
+	}, outputs)
+}
+
+func TestAcceptorLogic_HandleRequestVote__With_Log_Entries__With_Limit(t *testing.T) {
+	s := newAcceptorLogicTest()
+
+	s.putMembers()
+
+	s.log.UpsertEntries(
+		newPosLogEntries(2,
+			s.newCmd("cmd test 01"),
+			s.newCmd("cmd test 02"),
+			s.newCmd("cmd test 03"),
+		),
+	)
+
+	s.initLogic(2)
+
+	outputs := s.doHandleVote(s.currentTerm, 2)
+	assert.Equal(t, []RequestVoteOutput{
+		{
+			Success: true,
+			Term:    s.currentTerm,
+			Entries: []VoteLogEntry{
+				{Pos: 2, Entry: s.newCmd("cmd test 01")},
+				{Pos: 3, Entry: s.newCmd("cmd test 02")},
+			},
+		},
+		{
+			Success: true,
+			Term:    s.currentTerm,
+			Entries: []VoteLogEntry{
+				{Pos: 4, Entry: s.newCmd("cmd test 03")},
+				{Pos: 5, IsFinal: true},
 			},
 		},
 	}, outputs)
